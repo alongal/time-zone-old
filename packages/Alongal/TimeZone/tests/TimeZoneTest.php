416363730ee9d263b003187c538b9a9e1af2a4aa
alongal <alondagal@gmail.com>
2019-12-05 03:44:47 +1100

    added second test to check the time returned from the TimeZone getTime function

--- packages/Alongal/TimeZone/tests/TimeZoneTest.php
+++ packages/Alongal/TimeZone/tests/TimeZoneTest.php
@@ -3,6 +3,7 @@
 namespace Alongal\Tests;
 
 use Alongal\TimeZone\TimeZone;
+use Illuminate\Support\Carbon;
 use Orchestra\Testbench\TestCase;
 
 class TimeZoneTest extends TestCase
@@ -20,6 +21,24 @@ class TimeZoneTest extends TestCase
                 $melbourneCoordinates['latitude'],
                 $melbourneCoordinates['longitude']
             )->getTimezone()
+        );
+    }
+
+    /** @test */
+    function it_can_return_the_time_at_given_coordinates_with_default_format_Y_m_d_H_i_s()
+    {
+        $melbourneCoordinates = [
+            'latitude' => -38.1081,
+            'longitude' => 145.306
+        ];
+
+        Carbon::setTestNow(Carbon::createFromTimeString('2000-01-01 10:00:00', 'Australia/Melbourne'));
+
+        $this->assertEquals('2000-01-01 10:00:00',
+            TimeZone::createFromCoordinates(
+                $melbourneCoordinates['latitude'],
+                $melbourneCoordinates['longitude']
+            )->getTime()
         );
     }
 }
